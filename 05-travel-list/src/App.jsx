e124baa90ee724ea118b0f8b6a1eaf0228d7e859
Saujal Karki <saujalkarki04@gmail.com>
2023-11-21 21:17:34 +0545

    updated

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -21,25 +21,53 @@ function Logo() {
 }
 
 function Form() {
-  const [inp, setinp] = useState("someyhing");
+  const [descp, setDescp] = useState("");
+  const [quant, setQuant] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!descp) return;
+
+    const newItems = {
+      id: initialItems.length + 1,
+      description: descp,
+      quantity: quant,
+      packed: false,
+    };
+
+    initialItems.push(newItems);
+
+    setDescp("");
+    setQuant(1);
+
+    console.log(newItems);
+    console.log(initialItems);
   }
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="quantity" id="quantity">
+      <select
+        name="quantity"
+        id="quantity"
+        value={quant}
+        onChange={(e) => setQuant(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((opt) => (
           <option value={opt} key={opt}>
             {opt}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." value={inp} />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={descp}
+        onChange={(e) => setDescp(e.target.value)}
+      />
       <button>add</button>
-    </div>
+    </form>
   );
 }
 
