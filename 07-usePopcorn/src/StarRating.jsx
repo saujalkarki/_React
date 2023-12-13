2d36d46ecb6524ce7e8b21d417948cbefce3add9
Saujal Karki <saujalkarki04@gmail.com>
2023-12-13 23:11:53 +0545

    understanding how react works

--- 07-usePopcorn/src/StarRating.jsx
+++ 07-usePopcorn/src/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const mainStyle = {
   display: "flex",
@@ -10,8 +11,22 @@ const subStyle = {
   gap: "5px",
 };
 
-export function StarRating({ maxRatings = 3 }) {
-  const [rating, setRating] = useState(0);
+StarRating.propTypes = {
+  maxRatings: PropTypes.number.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.string,
+  message: PropTypes.array,
+  defaultRating: PropTypes.number,
+};
+
+export function StarRating({
+  maxRatings = 3,
+  color = "#FFD700",
+  size = "30px",
+  message = [],
+  defaultRating = 0,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rate) {
@@ -28,22 +43,29 @@ export function StarRating({ maxRatings = 3 }) {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p>{tempRating || rating || ""}</p>
+      <p>
+        {message.length >= maxRatings
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "30px",
-  height: "30px",
-  display: "block",
-  cursor: "pointer",
-};
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    width: "30px",
+    height: "30px",
+    display: "block",
+    cursor: "pointer",
+    color: `${color}`,
+  };
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
     <span
       style={starStyle}
@@ -56,8 +78,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           role="button"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#FFD700"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -66,7 +88,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- 07-usePopcorn/src/main.jsx
+++ 07-usePopcorn/src/main.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-// import App from "./App.jsx";
-// import "./index.css";
+// import StarRating from "./StarRating";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRatings={5} />
-    <StarRating maxRatings={10} />
-    <StarRating />
+    <App />
+    {/* <StarRating
+      maxRatings={5}
+      message={["bad", "okay", "satisfactory", "good", "excellent"]}
+      defaultRating={2}
+    />
+    <StarRating
+      maxRatings={10}
+      message={["bad", "okay", "satisfactory", "good", "excellent"]}
+      defaultRating={4}
+    />
+    <StarRating
+      maxRatings={3}
+      message={["bad", "okay", "satisfactory", "good", "excellent"]}
+      defaultRating={1}
+    />
+    <StarRating
+      maxRatings={5}
+      color="green"
+      message={["bad", "okay", "satisfactory", "good", "excellent"]}
+      defaultRating={3}
+    /> */}
   </React.StrictMode>
 );
