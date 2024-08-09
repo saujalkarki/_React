aef7b5b7465251c3776622e30bbc26cf819ccbbe
saujalkarki <saujalkarki04@gmail.com>
2024-08-09 23:47:37 +0545

    basics on redux

--- redux/index.js
+++ redux/index.js
@@ -1,15 +1,30 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+import logger from "redux-logger";
+
+// action name constants
+const inc = "increment";
+const dec = "decrement";
+const incByAmt = "incrementByAmount";
 
 // store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger.default));
+
 let history = [];
 
 // reducer
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === "increment") {
+  if (action.type === inc) {
     // immutable
     // state.amount = state.amount + 1;
     return { amount: state.amount + 1 };
+  }
+
+  if (action.type === dec) {
+    return { amount: state.amount - 1 };
+  }
+  if (action.type === incByAmt) {
+    return { amount: state.amount + action.payload };
   }
 
   return state;
@@ -23,10 +38,26 @@ store.subscribe(() => {
   console.log(history);
 });
 
+// Action Creators
+function increment() {
+  return {
+    type: inc,
+  };
+}
+function decrement() {
+  return {
+    type: dec,
+  };
+}
+function incrementByAmount(value) {
+  return {
+    type: incByAmt,
+    payload: value,
+  };
+}
+
 setInterval(() => {
-  store.dispatch({
-    type: "increment",
-  });
+  store.dispatch(incrementByAmount(10));
 }, 2000);
 
 console.log(store.getState());
